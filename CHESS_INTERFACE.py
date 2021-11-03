c79d7adf65bb7a170109bb6ba7d3c6e0cec4c9f3
Felicity Olivia Ogoke <79579227+misprout@users.noreply.github.com>
2021-11-03 13:12:34 +0100

    Create CHESS_INTERFACE.py

---
+++ CHESS_INTERFACE.py
@@ -0,0 +1,69 @@
+# ## Stand alone apps interface with tkinter based on Romans Prototype :)
+
+import tkinter as tk
+from tkinter.simpledialog import askstring
+import sqlite3
+
+root = tk.Tk()
+a = ("Comic Sans MS", 15, "bold")
+b = ("Comic Sans MS", 20, "bold")
+root.geometry("600x400")
+root.resizable(False, False)
+root.iconbitmap("chess_piece_knight.ico")
+
+
+
+
+
+class FirstPage(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.Widgets()
+        self.CREATETABLEFUNC()
+
+    def Widgets(self):
+        # pop up box asking for player usernames
+        self.player_name = askstring('Name', 'Please enter your player name:')
+        print(self.player_name)
+
+        if self.player_name:  # if user clicks ok its saved to database, else, its not
+            self.SAVETOTABLE()
+
+
+    def CREATETABLEFUNC(self):
+        self.Name = self.player_name
+
+        # Connect to database;
+        conn = sqlite3.connect('UserHistory.db')
+        cursor = conn.cursor()
+
+        # Create a table to store the user's player name;
+        cursor.execute("CREATE TABLE IF NOT EXISTS History_table (player NULL)")
+
+
+        # Close connection;
+        conn.commit()
+        conn.close()
+
+    def SAVETOTABLE(self):
+
+        # Connect to database;
+        conn = sqlite3.connect('UserHistory.db')
+        cursor = conn.cursor()
+
+        # save user's player name entry to table;
+
+        cursor.execute("INSERT INTO History_table(player) VALUES (:player_name)", {'player_name': self.player_name})
+
+        # Close connection;
+        conn.commit()
+        conn.close()
+
+
+
+
+FirstPage(root)
+
+
+
+root.mainloop()

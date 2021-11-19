55358b1de84472f839149e4b6be238a1cef9b348
Wochda <79580061+Wochda@users.noreply.github.com>
2021-11-19 21:43:03 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -48,7 +48,6 @@ def get_player_id():
 def get_move_ids():
     list1 = []
     list3 = []
-
     for line in data:
         x = str(re.findall("\}.*?\{|\][0-9]*\. .*?\{|\} [0-9]*\. .*?\{", line))
         y = re.sub("([']|\])|,|{|}|\.\.|\[", ",", x)
@@ -60,80 +59,93 @@ def get_move_ids():
             list1.append(a)
         list3.append(list1)
         list1 = []
-
-    list2 = []
-    list4 = []
     list5 = []
 
     for itemlist in list3:
-        for i in range(0, len(itemlist)):
-            try:
-                re.findall("[0-9]*\.", itemlist[i])[0]
-                continue
-            except:
+        list2 = []
+        listg = []
+        list4 = []
+        for i in range(0, len(itemlist)-1):
+            g = re.findall("[0-9]*\.", itemlist[i])
+            listg.append(g)
+        listg.insert(len(listg), ["0."])
+        for i in range(0, len(itemlist)-1):
+            if listg[i] == []:
                 list2.append(itemlist[i])
-                try:
-                    re.findall("[0-9]*\.", itemlist[i+1])[0]
+            if listg[i+1] != []:
+                if list2 != [] and len(list2) == 4:
                     list4.append(list2)
                     list2 = []
-                except:
-                    try:
-                        itemlist[i+1]
-                    except:
-                        list4.append(list2)
+                    continue
         if list4 != []:
             list5.append(list4)
-        list4 = []
     return list5
 
 def get_piece_ids(moves):
     list5 = []
     for movelistgame in moves:
         for movelistround in movelistgame:
-            for move in movelistround:
-                if move == "R":
-                    list5.append("R")
-                    continue
-                if move == "T":
-                    list5.append("T")
-                    continue
-                if move == "S":
-                    list5.append("S")
-                    continue
+                for move in movelistround:
+                    if move == "R":
+                        list5.append("R")
+                        continue
+                    if move == "T":
+                        list5.append("T")
+                        continue
+                    if move == "S":
+                        list5.append("S")
+                        continue
+                    if move == "T#":
+                        list5.append("T#")
 
-                try:
-                    r = re.search('O-O-O|O-O|^[A-Z]', move).group(0)
-                except:
-                    r = ''
-                if r == '':
-                    list5.append('Pawn')
-                if r == 'B':
-                    list5.append('Bishop')
-                if r == 'N':
-                    list5.append('Knight')
-                if r == 'Q':
-                    list5.append('Queen')
-                if r == 'K':
-                    list5.append('King')
-                if r == 'R':
-                    list5.append('Rook')
-                if r == 'O-O':
-                    list5.append('Short_Castle')
-                if r == 'O-O-O':
-                    list5.append('Long_Castle')
+                    try:
+                        r = re.search('O-O-O|O-O|^[A-Z]', move).group(0)
+                    except:
+                        r = ''
+                    if r == '':
+                        list5.append('Pawn')
+                    if r == 'B':
+                        list5.append('Bishop')
+                    if r == 'N':
+                        list5.append('Knight')
+                    if r == 'Q':
+                        list5.append('Queen')
+                    if r == 'K':
+                        list5.append('King')
+                    if r == 'R':
+                        list5.append('Rook')
+                    if r == 'O-O':
+                        list5.append('Short_Castle')
+                    if r == 'O-O-O':
+                        list5.append('Long_Castle')
     return list5
 
-def get_time():
+def get_time(moves):
     timelist = []
     timelistout = []
-    f = 0
+    temp = []
+
     for line in data:
         a = re.findall("\{ .*? \}", line)
         b = re.findall("[0-9]*:[0-9]*:[0-9]*\.[0-9]*", str(a))
         if b != []:
             timelist.append(b)
 
+    i = 0
     for item in timelist:
+        temp2 = []
+        v = 0
+        for move in moves[i]:
+            v += len(move)
+        for timeitem in item:
+            if len(temp2) >= v:
+                break
+            temp2.append(timeitem)
+        i += 1
+        temp.append(temp2)
+
+    for item in temp:
+        timelistout.insert(len(timelistout), 0)
         for i in range(0, len(item)-1):
             time1 = item[i]
             time2 = item[i+1]
@@ -154,62 +166,56 @@ def get_time():
             dateTimeDifference = dateTimeA - dateTimeB
             dateTimeDifferenceInSecs = dateTimeDifference.total_seconds()
             timelistout.append(dateTimeDifferenceInSecs)
-        timelistout.insert(0, 0)
     return timelistout
 
 
-with open('solo.txt', 'r') as data:
+with open('ffa.txt', 'r') as data:
     game_ids = get_game_id()
     data.close()
+    print(len(game_ids), "gameids")
 
-with open('solo.txt', 'r') as data:
+with open('ffa.txt', 'r') as data:
     player_ids = get_player_id()
     data.close()
+    print(len(player_ids), "playerids")
 
-with open('solo.txt', 'r') as data:
+with open('ffa.txt', 'r') as data:
     move_ids = get_move_ids()
     data.close()
+    u = 0
+    for movelistgame in move_ids:
+        for movelistround in movelistgame:
+                for move in movelistround:
+                    u += 1
+    print(u, "moves")
 
-with open('solo.txt', 'r') as data:
+with open('ffa.txt', 'r') as data:
     pieces = get_piece_ids(move_ids)
     data.close()
+    print(len(pieces), "pieces")
 
-with open('solo.txt', 'r') as data:
-    time = get_time()
+with open('ffa.txt', 'r') as data:
+    time = get_time(move_ids)
     data.close()
+    print(len(time), "time")
 
 db = Database()
 db.create_table()
+
 z = 0
 for i in range(0, len(game_ids)):
-    p = 0
     for o in range(0, len(move_ids[i])):
-        g = 0
-        while g < len(move_ids[i][o]):
-            if len(move_ids[i][o]) < 4:
-                p += 1
+            for x in range(0, len(move_ids[i][o])):
+                db.insert(game_ids[i], player_ids[i][x], move_ids[i][o][x], pieces[z], o+1, time[z])
+                #print(game_ids[i])
+                #print(player_ids[i][x])
+                #print(move_ids[i][o][x])
+                #print(o+1)
+                #print(pieces[z])
+                #print(time[z])
                 z += 1
-                g += 1
-                continue
-            if len(move_ids[i][o]) > 4:
-                move_ids[i][o].remove(move_ids[i][o][0])
-                if g > 0:
-                    g -= 1
-                continue
-            if p > 3:
-                p = 0
-            db.insert(game_ids[i], player_ids[i][p], move_ids[i][o][g], pieces[z], o+1, time[z])
-            db.commit()
-            #print(game_ids[i], player_ids[i][p], move_ids[i][o][g], pieces[z], o+1, time[z])
-            #print(game_ids[i])
-            #print(player_ids[i][p])
-            #print(move_ids[i][o][g])
-            #print(pieces[z])
-            #print(o+1)
-            #print(time[z])
-            #print(pieces[r])
-            #print(o + 1)
-            #print(time[g])
-            p += 1
-            z += 1
-            g += 1
+      #  except:
+       #     break
+    print("Game ", i, " done...")
+db.commit()
+db.close()

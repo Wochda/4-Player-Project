357dad8d39d153e582e28702a4a975d3ebd954cc
Felicity Olivia Ogoke <79579227+misprout@users.noreply.github.com>
2021-11-20 19:26:44 +0100

    Update CHESS_INTERFACE_TKINTER.py

--- CHESS_INTERFACE_TKINTER.py
+++ CHESS_INTERFACE_TKINTER.py
@@ -11,7 +11,7 @@ root = tk.Tk()
 a = ("Comic Sans MS", 15, "bold")
 b = ("Andale Mono", 20, "bold")
 root.geometry("800x600")
-# root.resizable(False, False)
+root.resizable(False, False)
 root.iconbitmap("chess_piece_knight.ico")
 img = ImageTk.PhotoImage(Image.open("chess_img.jpg"))
 imagelabel = tk.Label(root, image=img).grid(row=0, column=0, columnspan=2, rowspan=4)
@@ -48,7 +48,8 @@ class SecondPage(tk.Frame):
 
     # second page consists of 2 frames
     def Widgets(self):
-        
+
+    # MAKING BASIC LAYOUT;
         framebox_object0 = tk.Frame(root, width=500, height=50, bg="green")
         framebox_object1 = tk.Frame(root, width=100, height=500, bg="red")
         framebox_object2 = tk.Frame(root, width=400, height=500, bg="blue")
@@ -62,7 +63,28 @@ class SecondPage(tk.Frame):
         framebox_object0.grid(row=0, column=0, sticky="nswe", columnspan=2)
         framebox_object1.grid(row=1, column=0, sticky="nswe")
         framebox_object2.grid(row=1, column=1, sticky="nswe")
-        framebox_object3.grid(row=2, column=0, sticky="nswe",columnspan=2)
+        framebox_object3.grid(row=2, column=0, sticky="nswe", columnspan=2)
+
+    # LABELING:
+        Label1 = tk.Label(framebox_object0, text="Player Movements", font=b)
+        Label1.grid(row=0, column=0, sticky="nswe", columnspan=1)
+
+        Label2 = tk.Label(framebox_object0, text="Player Prediction", font=b)
+        Label2.grid(row=0, column=1, sticky="nswe", columnspan=1,padx=326)
+
+    # BUTTONS:
+        Button1 = tk.Button(framebox_object3, text='↩Restart', font=b)
+        Button1.grid(row=2, column=0, sticky="nswe", columnspan=1)
+
+        Button2 = tk.Button(framebox_object3, text='  Help  ', font=b)
+        Button2.grid(row=2, column=1, sticky="nswe", columnspan=1, padx= 210)
+
+        Button3 = tk.Button(framebox_object3, text='  End > ', font=b)
+        Button3.grid(row=2, column=2, sticky="nswe", columnspan=1, padx=2)
+        
+    
+
+
 
 
 root.mainloop()

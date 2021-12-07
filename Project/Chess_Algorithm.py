1c95d9e45ffb11155c8a8ffb37e29ad5694dc719
Waldini333 <Patrick.Waldenhofer@gmail.com>
2021-12-07 21:47:04 +0100

    Update Chess_Algorithm.py

--- Project/Chess_Algorithm.py
+++ Project/Chess_Algorithm.py
@@ -19,7 +19,7 @@ db.close()
 executionTime = (time.time() - startTime)
 print(f" Executiontime in seconds: {round(executionTime, 2)}")
 
-print(f"numer of rows in raw dataframe: {len(df_raw)}")
+print(f"number of rows in raw dataframe: {len(df_raw)}")
 
 # random moves to test algorithm
 print(random.sample(set(df_raw["Move_id"]),k=20))
@@ -29,37 +29,46 @@ print(df_singlegame.head(20))
 def Algorithm(move):
 
     # "Algorithm"
+    # feeds is the set of inputmoves
     feeds = set()
     feed = ""
+    # predictions is a dict which stores Player name as a key and prediction percentage as value in the end
     predictions = {}
-
-    totalpoints = 0
+    
+    # total matching moves of df_raw
+    totalmoves = 0
+    # while loop waiting for Player enters new move and updates the predictions
     while feed != "EXIT":
-        #self.reply = ""
+        
         feed = move
 
-        # iterate pandas df by rows
+        # iterate pandas df by rows (df_raw is the total list of all moves played ever linked to player_name, so there are also moves multiple times
+        # depending on how often a player plays
         for entry in df_raw.itertuples():
-
+            # entry[2] == "Move_id"
             if feed == entry[2]:
                 feeds.add(feed)
 
-                # check if player is already in dict
+                # check if player is already in dict, entry[1] == Player_id
+                # if not create new key
                 if entry[1] not in predictions.keys():
                     predictions[entry[1]] = 1
                 else:
                     pass
                 # found move -> +1
                 predictions[entry[1]] = predictions.get(entry[1]) + 1
-                # sort dict descending
+        
+        # sort dict descending
         predictions2 = dict(sorted(predictions.items(), key=lambda x: x[1], reverse=True))
 
-        # get total amount of points
+        
         # calculate percentage
+        # create sum of all moves in totalmoves
         for key, value in predictions2.items():
-            totalpoints += value
-
+            totalmoves += value
+        # Dreisatz calculation and shifting floating point by 2 with *100
         for key, value in predictions2.items():
-            predictions2[key] = round(((100 * value) / totalpoints) * 100, 2)
-
-        return (sorted(predictions2.items(), key=operator.itemgetter(1), reverse=True)[:5])
+            predictions2[key] = round(((100 * value) / totalmoves) * 100, 2)
+            
+        # return best predictions
+        return (sorted(predictions2.items(), key=operator.itemgetter(1), reverse=True)[:4])
